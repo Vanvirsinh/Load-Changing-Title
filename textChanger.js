b1851db339fee065323904817aef05c3c684ef8c
Vanvirsinh Deora <deoravanvirsinh7773@gmail.com>
2022-04-25 18:14:09 +0530

    first commit

---
+++ textChanger.js
@@ -0,0 +1,34 @@
+
+var textChangerElmt = document.getElementById('textChanger');
+
+
+function textLoader(arr, time) {
+
+
+    var j = 0;
+
+    function wordLoader(word, timing) {
+
+        var i = 0;
+        var myInterval = setInterval(function () {
+            var wordArray = word.split('');
+            if (i < wordArray.length) {
+                textChangerElmt.innerText += wordArray[i];
+                i++;
+            } else {
+                i = 0;
+                textChangerElmt.innerText = '';
+                clearInterval(myInterval);
+                wordLoader(arr[j], timing);
+            }
+        }, timing);
+        if (j < arr.length - 1) {
+            j++;
+        } else {
+            j = 0;
+        }
+    }
+
+    wordLoader(arr[0], time);
+
+}
